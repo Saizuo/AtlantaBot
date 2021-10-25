b3c075ce01597973e4a025fc6b4039769d65b786
Saizuo <71127774+Saizuo@users.noreply.github.com>
2021-10-25 21:10:34 +0530

    e
    e

--- config.sample.js
+++ config.sample.js
@@ -1,32 +1,32 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: "ODUxNDU1NzEwOTE5MjYyMzA4.YL4h7A.uqq3B08a34fke1R1PUh3u01YexQ",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "876113814314164256", // The ID of the support server
+		logs: "877818945464336404", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
 		enabled: false, // whether the dashboard is enabled or not
 		secret: "XXXXXXXXXXX", // Your discord client secret
-		baseURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
+		baseURL: "https://mikuni.gq", // The base URl of the dashboard
 		logs: "XXXXXXXXXXX", // The channel ID of logs
 		port: 8080, // Dashboard port
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
-		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
+		failureURL: "https://mikuni.gq" // url on which users will be redirected if they click the cancel button (discord authentication)
 	},
-	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
-	prefix: "*", // The default prefix for the bot
+	mongoDB: "mongodb+srv://Saizuo:U8DBoSbkCMA88o3p@rpbot.jvt2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // The URl of the mongodb database
+	prefix: ">", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
 	embed: {
-		color: "#0091fc", // The default color for the embeds
-		footer: "Atlanta | Open Source" // And the default footer for the embeds
+		color: "#fc5dea", // The default color for the embeds
+		footer: "Sakura" // And the default footer for the embeds
 	},
 	/* Bot's owner informations */
 	owner: {
-		id: "XXXXXXXXXXX", // The ID of the bot's owner
-		name: "Androz#2091" // And the name of the bot's owner
+		id: "892415353773232129", // The ID of the bot's owner
+		name: "Saizuo" // And the name of the bot's owner
 	},
 	/* DBL votes webhook (optional) */
 	votes: {
@@ -51,17 +51,17 @@ module.exports = {
 	},
 	/* The others utils links */
 	others: {
-		github: "https://github.com/Androz2091", // Founder's github account
-		donate: "https://patreon.com/Androz2091" // Donate link
+		github: "https://github.com/Saizuo", // Founder's github account
+		donate: "https://patreon.com/Mikuni" // Donate link
 	},
 	/* The Bot status */
 	status: [
 		{
-			name: "@Atlanta help on {serversCount} servers",
+			name: "@Sakura In Anime Universe",
 			type: "LISTENING"
 		},
 		{
-			name: "my website : atlanta-bot.fr",
+			name: "With Anime Universe",
 			type: "PLAYING"
 		}
 	]
